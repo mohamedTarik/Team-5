64a8b271686e07d6761aac2b2dfeb10eb3432668
Hashem khaled <hashem_khaled33@yahoo.com>
2018-11-26 23:17:55 +0200

    changed hello to h

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,40 @@
 public class Main
 {
 
-    public static void main(String[] args) {
-        System.out.println("H");
+    public static void main(String[] args)
+    {
+
+
+
+
+
     }
 
+
+
+    public int countprime(int [] arr)  //20160273
+    {
+        int counter=0;
+
+        for(int i =0;i<arr.length;i++)
+        {
+            if(isPrime(arr[i]))
+            {
+                counter++;
+            }
+        }
+    return counter;
+    }
+    public static boolean isPrime(int number){
+        for(int i=2; i<number; i++){
+            if(number%i == 0){
+                return false; //number is divisible so its not prime
+            }
+        }
+        return true; //number is prime now
+    }
 }
+
+
+
+
