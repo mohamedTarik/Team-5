1fdfb02aca0eeb17d8d2d6bfadd446041d4d43fc
Saraelsayedshalaby <45360299+Saraelsayedshalaby@users.noreply.github.com>
2018-11-28 19:23:04 +0200

    add function palandrome

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,29 @@ public class Main
 
             }
             else if (x == 8) {
-
+public class Main
+{
+	public static boolean isPalindrome(String inputString){
+    int length  = inputString.length();
+    int i, begin, end, middle;
+    begin  = 0;
+    end    = length - 1;
+    middle = (begin + end)/2;
+		for (i = begin; i <= middle; i++) {
+			if (inputString.charAt(begin) == inputString.charAt(end)) {
+			   begin++;
+			   end--;
+			}else {
+				break;
+			}
+		}
+		if (i == middle + 1) {
+		  return true;
+		}else {
+		  return false;\\not palaindrome
+	    }  
+	}
+	
 
             }
             else if (x == 9) {
